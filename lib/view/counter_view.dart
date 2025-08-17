c6c724007c2f4b699190383a2f6b7f052a5042f6
kimberl <022200520c@uandina.edu.pe>
2025-08-16 20:28:47 -0500

    cambio e color

--- lib/view/counter_view.dart
+++ lib/view/counter_view.dart
@@ -8,34 +8,58 @@ class CounterView extends StatelessWidget {
 Widget build(BuildContext context) {
   final counterViewModel = Provider.of<CounterViewModel>(context);
   return Scaffold(
-    appBar: AppBar(title: Text('Contador Con MVVM'),
+    appBar: AppBar(
+      title: Text('Contador Con MVVM'),
+      backgroundColor: const Color.fromARGB(255, 243, 77, 229),
     ),
+    backgroundColor: const Color.fromARGB(255, 213, 177, 210),
     body: Center( 
       child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
       children: <Widget>[
-        Text("Contador:${counterViewModel.count}"),
+        Text(
+          "Contador:${counterViewModel.count}",
+           style: const TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(height: 20,),
         Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             ElevatedButton(
-          onPressed:() {  
+            onPressed:() {  
             counterViewModel.incremented();
-        },
-        child:Icon(Icons.add),
-        ), 
+            },
+            style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color.fromARGB(255, 151, 55, 189), // Color botón +
+                  
+            ),
+            child: const Icon(Icons.add, color: Colors.white)), 
             ElevatedButton(onPressed:() {
           counterViewModel.decremented();
-        },
-        child:Icon(Icons.remove),
-        ),
-            ElevatedButton(onPressed: (){
-           counterViewModel.reset();
-        }, 
-        child:const Text("Reseteo")), 
-
+            },
+            style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color.fromARGB(255, 151, 55, 189), // Color botón +
+                      
+                ),
+                child: const Icon(Icons.remove, color: Colors.white)),
           ],
-        )
-        
+        ),
+
+        const SizedBox(height: 20),
+        ElevatedButton(onPressed: (){
+           counterViewModel.reset();
+            },
+            style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.blue, // Color botón reset
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 30, vertical: 15,
+                    )),      
+
+                    child: const Text("Reseteo",style: TextStyle(color: Colors.white, fontSize: 16),),),
+
       ],
     )
     )
